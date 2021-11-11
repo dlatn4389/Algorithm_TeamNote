2b763bb8ef5571baee0d491e71d31d7a23e6e74e
dlatn <>
2021-11-11 07:36:41 +0000

    replit test

---
+++ graph/graph_initialize.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// adjacency matrix method
+// Assume that the maximum number of nodes is 10000.
+
+const int max_node = 10001;
+int graph[max_node][max_node];
+
+// n is number of edge, m is number of edge
+int n, m;
+
+void initialize_graph() {
+  cin >> n >> m;
+
+}
+
+int main() {
+  initialize_graph();
+}
